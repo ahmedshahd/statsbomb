9e031ee3b6d2add0208adfe36ee571aef2a28beb
ahmedshahd <ahmedshahd350@gmail.com>
2023-07-29 16:36:11 +0300

    adding global styles and svg to link

--- components/Link/Link.tsx
+++ components/Link/Link.tsx
@@ -44,6 +44,23 @@ export const Link = ({
       {...props}
     >
       {label}
+      <svg
+        width="16"
+        height="14"
+        viewBox="0 0 16 14"
+        fill="none"
+      >
+        <path
+          d="M13.4844 6.93945L1.48438 6.93945"
+          stroke="white"
+          stroke-linecap="round"
+        ></path>
+        <path
+          d="M10.8047 3.43945L14.3402 6.97499L10.8047 10.5105"
+          stroke="white"
+          stroke-linecap="round"
+        ></path>
+      </svg>
     </a>
   );
 };
