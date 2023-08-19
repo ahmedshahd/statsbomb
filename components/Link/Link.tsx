f87a68da4a0c3545214c5a04ea162b31b4ef46f4
ahmedshahd <ahmedshahd350@gmail.com>
2023-08-19 14:55:06 +0300

    adding as prop to link

--- components/Link/Link.tsx
+++ components/Link/Link.tsx
@@ -1,16 +1,15 @@
 // import { convertToValidUrlPath } from '@/utils';
+import { Icon } from '@iconify/react';
 import React from 'react';
-import { convertToValidUrlPath } from '../utils';
-
 type LinkProps = {
-
-
   varient: 'primary' | 'secondary' | 'tertiary';
 
   label: string;
- 
+
+  as?: React.ElementType;
   /**
    * Optional click handler
+   *
    */
   onClick?: () => void;
 };
@@ -18,27 +17,20 @@ type LinkProps = {
 /**
  * Primary UI component for user interaction
  */
-export const Link = ({varient,label, ...props }: LinkProps) => {
+export const Link = ({
+  as: Component = 'a',
+  varient,
+  label,
+  ...props
+}: LinkProps) => {
   return (
-    <a
-      className={`link link--${varient}`}
-
-      href={convertToValidUrlPath(label)}
-      {...props}
-    >
+    <Component className={`link link--${varient}`} {...props}>
       {label}
-      <svg width="16" height="14" viewBox="0 0 16 14" fill="none">
-        <path
-          d="M13.4844 6.93945L1.48438 6.93945"
-          stroke="white"
-          stroke-linecap="round"
-        ></path>
-        <path
-          d="M10.8047 3.43945L14.3402 6.97499L10.8047 10.5105"
-          stroke="white"
-          stroke-linecap="round"
-        ></path>
-      </svg>
-    </a>
+      <Icon
+        icon="carbon:arrow-right"
+        // width="16"
+        // height="14"
+      />
+    </Component>
   );
 };
